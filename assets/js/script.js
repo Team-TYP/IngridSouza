ff92da9a7dad5b35beeb873eeeaa14ca25e9a9df
Ingrid Souza <92730473+IngridsSilveira@users.noreply.github.com>
2022-04-27 11:39:22 -0300

    uptade

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+//botão de scroll
 function subirTela() {
     window.scrollTo({
         top: 0,
@@ -11,5 +12,15 @@ function botaoScroll() {
         document.querySelector('.subir').style.display = 'block';
     }
 }
-
 window.addEventListener('scroll', botaoScroll);
+
+//menu
+window.onload = function() {
+            document.querySelector(".menuMobile").addEventListener("click", function(){
+                if(document.querySelector(".menu nav ul").style.display == 'flex') {
+                    document.querySelector(".menu nav ul").style.display = 'none';
+                } else {
+                    document.querySelector(".menu nav ul").style.display = 'flex';
+                }
+            });
+        };
